2f608437dbecbdc4aea4d1994929fd39c41a1a39
zacode123 <zuliabegum0@gmail.com>
2025-07-12 11:13:09 +0530

    Update ota_updater.py

--- Tools/ota_updater.py
+++ Tools/ota_updater.py
@@ -1,1 +1,7 @@
+from PyModules.ota import OTAUpdater
+from Config.config import STA_SSID, STA_PASS, GIT_URL
 
+ota_updater = OTAUpdater(STA_SSID, STA_PASS, GIT_URL)
+
+def auto_update():
+  ota_updater.download_and_install_update_if_available()
