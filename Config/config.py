ccaa96b95e53e75f4ec0a67e32cb12e5fc4c6300
zacode123 <zuliabegum0@gmail.com>
2025-07-12 11:15:03 +0530

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -1,1 +1,10 @@
+# Wifi Configurations
+STA_SSID = "POCO X4 Pro 5G"
+STA_PASS = "wasim@123"
 
+# Agent Configurations
+GEMINI_API_KEY = "AIzaSyAyc_C_Vg7JWOf4Ks4gPIwPYtkuj-wWmRg"
+AI_MODEL = "gemini-2.0-flash"
+
+# OTA GITHUB API Configurations
+GIT_URL = "https://github.com/kevinmcaleer/ota_test/main/"
