8dba61d4114d2745d60d44258c2623c79b99f4b0
zacode123 <zuliabegum0@gmail.com>
2025-07-12 11:12:02 +0530

    Update boot.py

--- boot.py
+++ boot.py
@@ -1,1 +1,9 @@
-
+import os, machine
+import gc
+import webrepl
+import PyModules.gemini_agent as agent
+import Tools.ota_updater as ota
+ota.auto_update()
+webrepl.start()
+agent.connectWIFI()
+gc.collect()
